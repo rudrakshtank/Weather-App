b9e18baba3e21f5030f264c3cb26a7cf5cd1637f
Rudraksh Tank <24ucs052@lnmiit.ac.in>
2025-07-10 11:24:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function getWeather() {
   const city = document.getElementById('city').value;
 
   const apikey = '7cc1c6d99aa240f295a54616251007';
-  const url = `http://api.weatherapi.com/v1/current.json?key=7cc1c6d99aa240f295a54616251007&q=${city}&aqi=yes`;
+  const url = `http://api.weatherapi.com/v1/current.json?key=${apikey}&q=${city}&aqi=yes`;
 
   try{
     const response = await fetch(url);
