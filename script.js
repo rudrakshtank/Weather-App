497e240d3a5c503cc84ff09a5cd5dffafa863cd3
Rudraksh Tank <24ucs052@lnmiit.ac.in>
2025-07-10 11:08:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+async function getWeather() {
+  const city = document.getElementById('city').value;
+
+  const apikey = 'efadc877d2aa433dbca50942251007';
+  const url = `http://api.weatherapi.com/v1/current.json?key=${apikey}&q=${city}&aqi=yes`;
+
+  try{
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const location = data.location.name + ', ' + data.location.country;
+    const temp = data.current.temp_c + ' \u00B0C';
+    const condition = data.current.condition.text;
+    const wind = data.current.wind_kph + ' kph';
+    const humidity = data.current.humidity + '%';
+    const feels = data.current.feelslike_c + ' \u00B0C';
+
+    document.getElementById('weather').innerHTML = `
+      <h2>${location}</h2>
+      <p><strong>Temperature:</strong> ${temp}</p>
+      <p><strong>Feels Like:</strong> ${feels}</p>
+      <p><strong>Condition:</strong> ${condition}</p>
+      <p><strong>Wind:</strong> ${wind}</p>
+      <p><strong>Humidity:</strong> ${humidity}</p>
+    `;
+  } 
+  catch(err) {
+    document.getElementById('weather').innerHTML = `<p>Could not fetch weather data. Try again.</p>`;
+    console.error(err);
+  }
+}
